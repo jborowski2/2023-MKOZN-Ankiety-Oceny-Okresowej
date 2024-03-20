3160c584b0d43557003ac218284c29382b056107
Michał Wnorowski <82973@student.pb.edu.pl>
2024-03-20 16:49:53 +0100

    komisja i poprawione pliki

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace OOP
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{3.7.1}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
